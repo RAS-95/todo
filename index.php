8bb702f9302413fca0bb45f8d6eaf371fd0cd672
RAS-95 <rafiahammedsongram@gmail.com>
2020-09-13 23:56:48 +0600

    index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 <p class = "text-center m-4"> <b>Make & edit your daily routine here!</b></p>
 <div class ="container">
     <div class ="row">
-             <div class ="col-12 col-sm-12 col-md-4 col-lg-4">
+             <div class ="col-12 col-sm-12 col-md-col-lg-5">
                 <div class ="jumbotron bg-success text-center p-4 ">
                  <a href="create.php" class ="text-white text-decoration-none"> <h3>Create Task </h3></a>
                  </div>
              </div>  
              
-             <div class ="col-12 col-sm-12 col-md-4 col-lg-4">
+             <div class ="col-12 col-sm-12 col-md-col-lg-5">
                 <div class ="jumbotron bg-warning text-center p-4 ">
                  <a href="view.php" class ="text-white text-decoration-none"> <h3>View Task </h3></a>
                  </div>
